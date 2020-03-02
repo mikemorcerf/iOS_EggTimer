a46f00e5157692cc24c5a8b1f7bb445f085eb948
Michael Moura <mikemorcerf@gmail.com>
2020-03-01 19:54:20 -0500

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    let eggTimes = ["Soft": 3, "Medium": 5, "Hard": 7]
+    var timePassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    var player: AVAudioPlayer?
     
-
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var screenText: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        progressBar.progress = 0.00
+        timePassed = 0
+        totalTime = eggTimes[hardness]!
+        screenText.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateCounter() {
+        if timePassed < totalTime {
+            timePassed += 1
+            progressBar.progress = (Float(timePassed)/Float(totalTime))
+            
+        } else{
+            timer.invalidate()
+            screenText.text = "Done"
+            playAlarm()
+        }
+    }
+    
+    func playAlarm() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else {
+            print("url not found")
+            return
+        }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            player!.play()
+        } catch let error as NSError {
+            print("error: \(error.localizedDescription)")
+        }
+    }
 }
